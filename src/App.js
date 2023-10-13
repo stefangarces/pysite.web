bde451b2a181b7c710bf131b932c0ae7ced742ed
Stefan Garces <stefan.garces@volvo.com>
2023-10-13 11:08:03 +0200

    initinal commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import profile from './profile.png';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState(false)
+
+  useEffect(() => {
+    if (theme) {
+      document.body.style.backgoundColor = "#FFFFFF";
+    } else {
+      document.body.style.backgoundColor = "#000000";
+    }
+  }, [theme]);
+
+console.log(theme)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="App" background-color='#EBEBEB'>
+      <header className={theme ? "App-header-dark" : "App-header-light"}>
+        <img src={profile} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          An amazing site
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Don't click here
         </a>
+        <button onClick={() => setTheme((theme) => !theme)} >{theme ? 'Light' : 'Dark' } Mode</button>
       </header>
     </div>
   );
