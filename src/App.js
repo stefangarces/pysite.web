85c106fd435c8c13ebb3f345efed4e064f405561
Stefan Garces <stefan.garces@volvo.com>
2023-11-19 00:18:24 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
-import profile from './profile.png';
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import './styles/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import NotFound from './pages/error.js';
 
 function App() {
-  const [theme, setTheme] = useState(false)
-
-  useEffect(() => {
-    if (theme) {
-      document.body.style.backgoundColor = "#FFFFFF";
-    } else {
-      document.body.style.backgoundColor = "#000000";
-    }
-  }, [theme]);
-
-console.log(theme)
   return (
-    <div className="App" background-color='#EBEBEB'>
-      <header className={theme ? "App-header-dark" : "App-header-light"}>
-        <img src={profile} className="App-logo" alt="logo" />
-        <p>
-          An amazing site
-        </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Don't click here
-        </a>
-        <button onClick={() => setTheme((theme) => !theme)} >{theme ? 'Light' : 'Dark' } Mode</button>
-      </header>
-    </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="*" element={<NotFound/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
